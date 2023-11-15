51147da7c597a07d42d02adfb02f01879dfc2a28
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2023-11-15 20:11:00 +0300

    upload project

---
+++ index.js
@@ -0,0 +1,14 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName('mySlides');
+  slideIndex++;
+  for (i = 0; i < slides.length; i++) {  slides[i].style.display = 'none'; }
+  if (slideIndex >= slides.length) { slideIndex = 0; }
+  slides[slideIndex].style.display = 'block';
+  setTimeout(showSlides, 5000);
+}
+
+
