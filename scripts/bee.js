0a4305e04c4b52389b68c0a63de5edd446efe98f
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2023-12-11 18:30:45 +0300

    проверил eslintr

--- scripts/bee.js
+++ scripts/bee.js
@@ -1,26 +1,27 @@
-const bee = document.querySelectorAll('.image-container img')
-const trigger = document.getElementById('triggerWord')
-const textabouthelp = document.getElementById('TextAboutHelp')
-const video =    document.getElementById('videoContainer')
+const bee = document.querySelectorAll('.image-container img');
+const trigger = document.getElementById('triggerWord');
+const textabouthelp = document.getElementById('TextAboutHelp');
+const video = document.getElementById('videoContainer');
 const message = document.getElementById('message');
 
-bee.forEach(image => {
-    image.addEventListener('click', function() {
-        this.style.display = 'none';
-        checkAllImagesHidden();
-    });
-});
 function checkAllImagesHidden() {
-    const allHidden = Array.from(bee).every(img => img.style.display === 'none');
-    if (allHidden) {
-        message.style.display = 'block';
-    }
+  const allHidden = Array.from(bee).every((img) => img.style.display === 'none');
+  if (allHidden) {
+    message.style.display = 'block';
+  }
 }
-trigger.addEventListener('click', function() {
-    video.style.display = 'block';
-    textabouthelp.style.display = 'none';
-    message.style.display = 'none';
-    document.querySelectorAll('.image-container img').forEach(function(img) {
-        img.style.display = 'none';
-    });
+bee.forEach((image) => {
+  image.addEventListener('click', () => {
+    this.style.display = 'none';
+    checkAllImagesHidden();
+  });
+});
+
+trigger.addEventListener('click', () => {
+  video.style.display = 'block';
+  textabouthelp.style.display = 'none';
+  message.style.display = 'none';
+  document.querySelectorAll('.image-container img').forEach(() => {
+    this.style.display = 'none';
+  });
 });
--- scripts/dog.js
+++ scripts/dog.js
@@ -4,43 +4,39 @@ const dog = document.getElementById('dog');
 const meat = document.getElementById('meat');
 const hungryText = document.getElementById('hungryText');
 const thankYouText = document.getElementById('thankYouText');
-dog.addEventListener('mouseenter', function() {
-    if (allowMovementAndJumping) {
-        const screenWidth = window.innerWidth - this.clientWidth;
-        const screenHeight = window.innerHeight - this.clientHeight;
+dog.addEventListener('mouseenter', function () {
+  if (allowMovementAndJumping) {
+    const screenWidth = window.innerWidth - this.clientWidth;
+    const screenHeight = window.innerHeight - this.clientHeight;
 
-        const randomX = Math.random() * screenWidth;
-        const randomY = Math.random() * screenHeight;
+    const randomX = Math.random() * screenWidth;
+    const randomY = Math.random() * screenHeight;
 
-        hungryText.style.left = `${randomX}px`;
-        hungryText.style.top = `${randomY - hungryText.offsetHeight}px`;
+    hungryText.style.left = `${randomX}px`;
+    hungryText.style.top = `${randomY - hungryText.offsetHeight}px`;
 
-        this.style.left = `${randomX}px`;
-        this.style.top = `${randomY}px`;
-        this.style.right = '';
-        this.style.bottom = '';
-
-
+    this.style.left = `${randomX}px`;
+    this.style.top = `${randomY}px`;
+    this.style.right = '';
+    this.style.bottom = '';
     if (!this.classList.contains('jumping')) {
-        this.classList.add('jumping');
+      this.classList.add('jumping');
     }
     meat.style.display = 'block';
     hungryText.style.display = 'block';
-    }
+  }
 });
 
-
-meat.addEventListener('mouseenter', function() {
-    allowMovementAndJumping = false;
-    meat.style.display = 'none';
-    hungryText.style.display = 'none';
-    thankYouText.style.display = 'block';
-    thankYouText.style.right = '10px';
-    thankYouText.style.bottom = '100px';
-
-    dog.classList.remove('jumping');
-    dog.style.left = '';
-    dog.style.top = '';
-    dog.style.right = '10px';
-    dog.style.bottom = '10px';
+meat.addEventListener('mouseenter', () => {
+  allowMovementAndJumping = false;
+  meat.style.display = 'none';
+  hungryText.style.display = 'none';
+  thankYouText.style.display = 'block';
+  thankYouText.style.right = '10px';
+  thankYouText.style.bottom = '100px';
+  dog.classList.remove('jumping');
+  dog.style.left = '';
+  dog.style.top = '';
+  dog.style.right = '10px';
+  dog.style.bottom = '10px';
 });
