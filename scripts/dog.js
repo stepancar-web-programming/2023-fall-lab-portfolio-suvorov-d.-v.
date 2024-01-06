eb81365a5a82df23a1930a0f3b49de56b427f949
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2024-01-06 17:28:46 +0300

    привел к одному формату

--- scripts/dog.js
+++ scripts/dog.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   let allowMovementAndJumping = true; // Флаг теперь изолирован внутри IIFE
 
   const dog = document.getElementById('dog');
@@ -7,43 +7,43 @@
   const thankYouText = document.getElementById('thankYouText');
 
   dog.addEventListener('mouseenter', function () {
-  const dog = document.getElementById('dog');
-  const meat = document.getElementById('meat');
-  const hungryText = document.getElementById('hungryText');
-  const thankYouText = document.getElementById('thankYouText');
-  if (allowMovementAndJumping) {
-    const screenWidth = window.innerWidth - this.clientWidth;
-    const screenHeight = window.innerHeight - this.clientHeight;
+    const dog = document.getElementById('dog');
+    const meat = document.getElementById('meat');
+    const hungryText = document.getElementById('hungryText');
+    const thankYouText = document.getElementById('thankYouText');
+    if (allowMovementAndJumping) {
+      const screenWidth = window.innerWidth - this.clientWidth;
+      const screenHeight = window.innerHeight - this.clientHeight;
 
-    const randomX = Math.random() * screenWidth;
-    const randomY = Math.random() * screenHeight;
+      const randomX = Math.random() * screenWidth;
+      const randomY = Math.random() * screenHeight;
 
-    hungryText.style.left = `${randomX}px`;
-    hungryText.style.top = `${randomY - hungryText.offsetHeight}px`;
+      hungryText.style.left = `${randomX}px`;
+      hungryText.style.top = `${randomY - hungryText.offsetHeight}px`;
 
-    this.style.left = `${randomX}px`;
-    this.style.top = `${randomY}px`;
-    this.style.right = '';
-    this.style.bottom = '';
-    if (!this.classList.contains('jumping')) {
-      this.classList.add('jumping');
+      this.style.left = `${randomX}px`;
+      this.style.top = `${randomY}px`;
+      this.style.right = '';
+      this.style.bottom = '';
+      if (!this.classList.contains('jumping')) {
+        this.classList.add('jumping');
+      }
+      meat.style.display = 'block';
+      hungryText.style.display = 'block';
     }
-    meat.style.display = 'block';
-    hungryText.style.display = 'block';
-  }
-});
+  });
 
-meat.addEventListener('mouseenter', () => {
-  allowMovementAndJumping = false;
-  meat.style.display = 'none';
-  hungryText.style.display = 'none';
-  thankYouText.style.display = 'block';
-  thankYouText.style.right = '10px';
-  thankYouText.style.bottom = '100px';
-  dog.classList.remove('jumping');
-  dog.style.left = '';
-  dog.style.top = '';
-  dog.style.right = '10px';
-  dog.style.bottom = '10px';
-})
+  meat.addEventListener('mouseenter', () => {
+    allowMovementAndJumping = false;
+    meat.style.display = 'none';
+    hungryText.style.display = 'none';
+    thankYouText.style.display = 'block';
+    thankYouText.style.right = '10px';
+    thankYouText.style.bottom = '100px';
+    dog.classList.remove('jumping');
+    dog.style.left = '';
+    dog.style.top = '';
+    dog.style.right = '10px';
+    dog.style.bottom = '10px';
+  })
 })();
